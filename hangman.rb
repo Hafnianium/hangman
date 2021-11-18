468e5f07a09874d1e58170823669469646ff1122
patrick <patrick@o-neill.ca>
2021-11-18 11:21:47 -0500

    Board update working correctly

--- hangman.rb
+++ hangman.rb
@@ -12,14 +12,17 @@ end
 
 # generates the underscores that slowly reveal the word
 class Board
-  attr_reader :secretword
+  attr_reader :secretword, :secret_word_array
+  attr_accessor :secret_word_display_array
 
   def initialize(secretword)
     @secretword = secretword
+    @secret_word_array = secretword.secret_word.split('')
+    @secret_word_display_array = Array.new(secretword.secret_word.length, '_')
   end
 
   def display_string
-    puts Array.new(secretword.secret_word.length, '_').join(' ')
+    puts secret_word_display_array.join(' ')
   end
 end
 
@@ -60,18 +63,45 @@ end
 # instantiates the classes and starts the game
 class GameRunner
   attr_reader :secretword, :board, :player
+  attr_accessor :guesses
 
   def initialize
     @secretword = SecretWord.new
     @board = Board.new(secretword)
     @player = Player.new
+    @guesses = 0
     welcome_screen
-    player.input_letter
+    play_game
   end
 
   def welcome_screen
-    puts "Welcome to hangman #{player.name}! The secret word is #{secretword.secret_word.length} letters long."
+    puts "Welcome to hangman #{player.name}! The secret word is #{secretword.secret_word.length} letters long.
+You have #{10 - guesses} guesses to get the word."
     board.display_string
+  end
+
+  def update_display_string
+    board.secret_word_array.each_with_index do |element, index|
+      board.secret_word_display_array[index] = player.guessed_letters[-1] if element == player.guessed_letters[-1]
+    end
+  end
+
+  def guess_check
+    if board.secret_word_array.any?(player.guessed_letters[-1])
+      puts "Correct! #{player.guessed_letters[-1]} is present!"
+    else
+      @guesses += 1
+      puts "Sorry! #{player.guessed_letters[-1]} is not present \n#{10 - @guesses} guesses remaining."
+    end
+  end
+
+  def play_game
+    loop do
+      player.input_letter
+      guess_check
+      update_display_string
+      board.display_string
+    end
   end
 end
 
